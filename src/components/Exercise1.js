6ff0f879083f66b5e71a1ec347039a6899e9521a
ziv221 <ziv.itzh@gmail.com>
2020-10-05 13:46:39 +0300

    finished

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -8,14 +8,29 @@ class Exercise1 extends Component {
             name: "",
             age: ""
         }
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
+    handleInputChange(event){
+        const target = event.target
+        const value = target.type === 'checkbox' ? target.checked : target.value
+        const name = target.name
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    alertInfo = () =>{
+        alert(`Come in ${this.state.name}, you're ${this.state.age} - that's good enough`)
+    }
+    
     render() {
         return (
             <div>
-                <input id="name-input" />
-                <input id="age-input" />
-                <button>Go to Bar</button>
+                <input id="name-input" name="name" value={this.state.name} onChange={this.handleInputChange} />
+                <input id="age-input"  name="age"  value={this.state.age}  onChange={this.handleInputChange} />
+                <button onClick={this.alertInfo}>Go to Bar</button>
             </div>
         );
     }
--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -8,13 +8,28 @@ constructor() {
         name: "",
         fruit: ""
     }
+    this.handleChange = this.handleChange.bind(this)
 }
+handleChange = event =>{    
+    const target = event.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
+    this.setState({[name]: value}, ()=>{
+        console.log(`${this.state.name} selected ${this.state.fruit}`)
+    })
 
+}
+    
 render() {
     return (
         <div>
-            <input id="name-input" />
-            <select id="select-input"></select>
+            <input id="name-input" name="name" onChange={this.handleChange}/>
+            <select id="select-input" name="fruit" onChange={this.handleChange}>
+                <option value="Banana">Banana</option>
+                <option value="Kiwi">Kiwi</option>
+                <option value="Apple">Apple</option>
+                <option value="Orange ">Orange </option>
+            </select>
         </div>
     );
 }
